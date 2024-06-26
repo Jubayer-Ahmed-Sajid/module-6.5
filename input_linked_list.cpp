436e2943438fde03dba1431b959343a0e074c8c5
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-06-26 19:24:41 +0600

    linked list input program completed

--- input_linked_list.cpp
+++ input_linked_list.cpp
@@ -1,6 +1,65 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main (){
-    
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void print_node(Node *head)
+{
+    Node *temp = head;
+    cout << endl
+         << "Your nodes are" << " ";
+    if (head == NULL)
+    {
+        cout << endl
+             << "You do not have any Node!!" << endl
+             << endl;
+        return;
+    }
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+
+    Node *head = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_at_tail(head, val);
+    }
+    print_node(head);
+
     return 0;
 }
